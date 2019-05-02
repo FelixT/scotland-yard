8e225d7721b4b25a94511cf6a74a6802081dab38
f3 <felixfftt@gmail.com>
2019-05-02 11:27:07 +0100

    basic ai for mrx which finds location furthest from current position

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAI.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAI.java
@@ -7,10 +7,7 @@ import java.util.function.Consumer;
 
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
 import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
-import uk.ac.bris.cs.scotlandyard.model.Colour;
-import uk.ac.bris.cs.scotlandyard.model.Move;
-import uk.ac.bris.cs.scotlandyard.model.Player;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYardView;
+import uk.ac.bris.cs.scotlandyard.model.*;
 
 // TODO name the AI
 @ManagedAI("AI")
@@ -23,16 +20,45 @@ public class MyAI implements PlayerFactory {
 	}
 
 	// TODO A sample player that selects a random move
-	private static class MyPlayer implements Player {
+	private static class MyPlayer implements Player, MoveVisitor {
 
+		private int location;
 		private final Random random = new Random();
+		int furthestDistance = 0;
+		Move furthestMove = new PassMove(Colour.BLACK);
+
+		public void visit(TicketMove move) {
+			// get the distance away the move would be
+			int distance = Math.abs(location - move.destination());
+
+			if(distance > furthestDistance) {
+				furthestDistance = distance;
+				furthestMove = move;
+			}
+		}
+
+		public void visit(DoubleMove move) {
+			// get the distance away the move would be
+			int distance = Math.abs(location - move.finalDestination());
+
+			if(distance > furthestDistance) {
+				furthestDistance = distance;
+				furthestMove = move;
+			}
+		}
 
 		@Override
 		public void makeMove(ScotlandYardView view, int location, Set<Move> moves,
 				Consumer<Move> callback) {
-			// TODO do something interesting here; find the best move
-			// picks a random move
-			callback.accept(new ArrayList<>(moves).get(random.nextInt(moves.size())));
+			// reset furthest distance
+			furthestDistance = 0;
+			furthestMove = new PassMove(Colour.BLACK);
+            this.location = location;
+
+            for(Move move : moves)
+            	move.visit(this);
+
+			callback.accept(furthestMove);
 
 		}
 	}
